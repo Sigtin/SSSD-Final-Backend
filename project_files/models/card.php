c87fe23f6767a6fafa58b8067c275001b05584fb
Austin Sigler <asigler@student.neumont.edu>
2019-12-03 17:59:00 -0700

    Update card.php

--- project_files/models/card.php
+++ project_files/models/card.php
@@ -1,11 +1,16 @@
 <?php
     class Card {
+        private $id;
         private $name;
         private $imageUrl;
-        private $oracleText;
-        private $manaCost;
-        private $colorIdentity;
-        private $type;
+        
+        function set_id($id) {
+            $this->id = $id;
+        }
+
+        function get_id() {
+            return $this->id;
+        }
 
         function set_name($name) {
             $this->name = $name;
@@ -22,6 +27,13 @@
         function get_imageUrl() {
             return $this->imageUrl;
         }
+    }
+
+    class MTG_Card extends Card {
+        private $oracleText;
+        private $manaCost;
+        private $colorIdentity;
+        private $type;
 
         function set_oracleText($oracleText) {
             $this->oracleText = $oracleText;
@@ -53,6 +65,84 @@
 
         function get_type() {
             return $this->type;
+        }
+    }
+
+    class Pokemon_Card extends Card {
+        private $attacks;
+        private $hp;
+        private $types;
+        private $weaknesses;
+
+        function set_attacks($attacks) {
+            $this->attacks = $attacks
+        }
+
+        function get_attacks() {
+            return $this->attacks;
+        }
+
+        function set_hp($hp) {
+            $this->hp = $hp;
+        }
+
+        function get_hp() {
+            return $this->hp;
+        }
+
+        function set_types($types) {
+            $this->types = $types;
+        }
+
+        function get_types() {
+            return $this->types;
+        }
+
+        function set_weaknesses($weaknesses) {
+            $this->weaknesses = $weaknesses;
+        }
+
+        function get_weaknesses() {
+            return $this->weaknesses;
+        }
+    }
+
+    class Yugioh_Card extends Card {
+        private $type;
+        private $atk;
+        private $def;
+        private $description;
+
+        function set_type($type) {
+            $this->type = $type;
+        }
+
+        function get_type() {
+            return $this->type;
+        }
+
+        function set_atk($atk) {
+            $this->atk = $atk;
+        }
+
+        function get_atk() {
+            return $this->atk;
+        }
+
+        function set_def($def) {
+            $this->def = $def;
+        }
+
+        function get_def() {
+            return $this->def;
+        }
+
+        function set_description($description) {
+            $this->description = $description;
+        }
+
+        function get_description() {
+            return $this->description;
         }
     }
 ?>
